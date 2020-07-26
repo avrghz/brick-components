c834ef8aecb675b54624daab0d6c012d25bddb2f
AswinTom Varghese <aswin.thunchathu@gmail.com>
2020-07-26 09:46:48 +0200

    [Checkbox] Added keyboard focus

--- src/components/CheckboxWrapper/index.tsx
+++ src/components/CheckboxWrapper/index.tsx
@@ -1,4 +1,5 @@
-import { Component, h, Prop } from '@stencil/core'
+import { Component, h, Prop, Host, Element } from '@stencil/core'
+import KeyboardFocus from '../../shared/decorators/keyboardFocus'
 
 @Component({
     tag: 'bk-checkbox-wrapper',
@@ -6,17 +7,21 @@ import { Component, h, Prop } from '@stencil/core'
     styleUrl: './index.scss',
 })
 export class CheckboxWrapper {
+    @KeyboardFocus() @Element() el!: HTMLElement
+
     @Prop() label!: string
 
     render() {
         return (
-            <label class="bk-checkbox">
-                <slot></slot>
-                <span class="bk-checkbox__input">
-                    <span class="bk-checkbox__inner"></span>
-                </span>
-                <span class="bk-checkbox__label">{this.label}</span>
-            </label>
+            <Host>
+                <label class="bk-checkbox">
+                    <slot></slot>
+                    <span class="bk-checkbox__input">
+                        <span class="bk-checkbox__inner"></span>
+                    </span>
+                    <span class="bk-checkbox__label">{this.label}</span>
+                </label>
+            </Host>
         )
     }
 }
