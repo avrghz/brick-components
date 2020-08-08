b28d66ee19694e75d155e7eff6f1d905b88422fc
AswinTom Varghese <aswin.thunchathu@gmail.com>
2020-08-08 21:54:12 +0200

    [Theme] Fixed dropdown list dark mode

--- src/components/LazyMedia/LazyMedia.stories.tsx
+++ src/components/LazyMedia/LazyMedia.stories.tsx
@@ -29,7 +29,7 @@ export const Image = () => {
     }
 
     return html` <bk-lazy-media>
-        <div class="bk-row">
+        <div class="bk-row is-no-gutter">
             ${images}
         </div>
         ${courtesy}
@@ -70,7 +70,7 @@ export const Picture = () => {
     }
 
     return html` <bk-lazy-media>
-        <div class="bk-row">
+        <div class="bk-row is-no-gutter">
             ${images}
         </div>
         ${courtesy}
@@ -100,7 +100,7 @@ export const BackgroundImage = () => {
 
     return html`
         <bk-lazy-media>
-            <div class="bk-row">
+            <div class="bk-row is-no-gutter">
                 ${images}
             </div>
             ${courtesy}
